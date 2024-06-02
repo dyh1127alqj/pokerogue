f81cee3f4ce39ae2daf0d19339ce4e405070a005
djd <dyh1127alqj@163.com>
2024-06-02 12:54:06 +0800

    修改单数

--- src/data/pokeball.ts
+++ src/data/pokeball.ts
@@ -55,13 +55,13 @@ export function getPokeballName(type: PokeballType): string {
 export function getPokeballCatchMultiplier(type: PokeballType): number {
   switch (type) {
     case PokeballType.POKEBALL:
-      return 1;
+      return -1;
     case PokeballType.GREAT_BALL:
-      return 1.5;
+      return 15;
     case PokeballType.ULTRA_BALL:
-      return 2;
+      return 20;
     case PokeballType.ROGUE_BALL:
-      return 3;
+      return 30;
     case PokeballType.MASTER_BALL:
       return -1;
     case PokeballType.LUXURY_BALL:
